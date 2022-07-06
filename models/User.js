b63f0476385bd874848005945021a8766458feb1
Joey Binas <joseph.t.binas@gmail.com>
2022-07-06 15:21:43 -0700

    tested with consolelog

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/connection');
+const sequelize = require('./config/connection');
 
 class User extends Model {
   checkPassword(loginPw) {
+    //
+    console.log(loginPW, this.password);
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
