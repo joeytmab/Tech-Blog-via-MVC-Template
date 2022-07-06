a584a53fe6a805774376bb3c6acbf3724c0eddf9
Joey Binas <joseph.t.binas@gmail.com>
2022-07-06 15:19:33 -0700

    redid starter code for postdata

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
-const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const sequelize = require('./config/connection');
+const { User, Post, Comment } = require('./models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,10 +13,15 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
     });
   }
 
