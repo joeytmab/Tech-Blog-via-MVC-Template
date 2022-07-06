15326badbd971615987be6f6b6a711d7f89e1819
Joey Binas <joseph.t.binas@gmail.com>
2022-07-06 15:23:18 -0700

    redid index, userroute

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -11,6 +11,7 @@ router.post('/', async (req, res) => {
 
       res.status(200).json(userData);
     });
+
   } catch (err) {
     res.status(400).json(err);
   }
@@ -21,22 +22,12 @@ router.post('/login', async (req, res) => {
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-
-    req.session.save(() => {
+   
+  req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
       
@@ -53,6 +44,7 @@ router.post('/logout', (req, res) => {
     req.session.destroy(() => {
       res.status(204).end();
     });
+    
   } else {
     res.status(404).end();
   }
