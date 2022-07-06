972ca6490b664245d974449295c7956e34e3f6fe
Joey Binas <joseph.t.binas@gmail.com>
2022-07-06 16:22:27 -0700

    fixed routing pathways

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post, Comment, User } = require('./models');
+const { Post, Comment, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 // GET all posts for homepage
 router.get('/', async (req, res) => {
